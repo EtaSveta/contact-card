ed7bb242673aad862364eb5046613796ee0798bd
Svetlana Nesterova <sveta59n@yahoo.com>
2022-12-04 21:35:18 -0500

    delete

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,4 +1,4 @@
-import { initdb, getDb, postDb, deleteDb } from './database';
+import { initdb, getDb, postDb, deleteDb,editDb } from './database';
 import { fetchCards } from './cards';
 import { toggleForm, clearForm } from './form';
 
@@ -47,6 +47,13 @@ form.addEventListener('submit', event => {
   if (submitBtnToUpdate == false) {
     postDb(name, email, phone, profile);
   } else {
+    // Obtains values passed into the form element
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let profile = document.querySelector('input[type="radio"]:checked').value;
+    // Calls the editDB function passing in any values from the form element as well as the ID of the contact that we are updating
+    editDb(profileId, name, email, phone, profile);
 
     fetchCards();
     // Toggles the submit button back to POST functionality
@@ -63,10 +70,29 @@ form.addEventListener('submit', event => {
 
 window.deleteCard = (e) => {
   // Grabs the id from the button element attached to the contact card.
-    let id = parseInt(e.id);
-    // Delete the card
-    deleteDb(id);
-    // Reload the DOM
-    fetchCards();
-  };
+  let id = parseInt(e.id);
+  // Delete the card
+  deleteDb(id);
+  // Reload the DOM
+  fetchCards();
+};
+
+window.editCard = (e) => {
+  // Grabs the id from the button element attached to the contact card and sets a global variable that will be used in the form element.
+  profileId = parseInt(e.dataset.id);
+
+  // Grabs information to pre-populate edit form
+  let editName = e.dataset.name;
+  let editEmail = e.dataset.email;
+  let editPhone = e.dataset.phone;
+
+  document.getElementById("name").value = editName;
+  document.getElementById("email").value = editEmail;
+  document.getElementById("phone").value = editPhone;
+
+  form.style.display = "block";
+
+  // Toggles the Submit button so that it now Updates an existing contact instead of posting a new one
+  submitBtnToUpdate = true;
+};
 
